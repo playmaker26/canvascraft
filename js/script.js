d191bfe2e7e54d622947e3c437379af72d597d40
playmaker26 <jorge.artaza22@gmail.com>
2024-08-27 13:45:16 -0400

    Started with JavaScript interaction

--- js/script.js
+++ js/script.js
@@ -1,1 +1,80 @@
-const canvas = document.querySelector('#drawingCanvas');
+function resizeCanvas() {
+    const canvas = document.querySelector('#drawingCanvas');
+    const sidebarWidth = 150;
+    const availableWidth = window.innerWidth - sidebarWidth;
+    const availableHeight = window.innerHeight;
+
+    canvas.width = availableWidth;
+    canvas.height = availableHeight;
+}
+window.addEventListener('resize', resizeCanvas);
+
+resizeCanvas();
+
+function tools() {
+    const canvas = document.querySelector('#drawingCanvas');
+    const context = canvas.getContext('2d');
+    context.strokeStyle = 'black';
+    let pencil = document.querySelector('.pencil');
+    let eraser = document.querySelector('.eraser');
+    let shape = document.querySelector('.shape');
+    let color = document.querySelector('.color');
+    let fill = document.querySelector('.fill');
+    let text = document.querySelector('.text');
+    let undo = document.querySelector('.undo');
+    let redo = document.querySelector('.redo');
+    let zoomIn = document.querySelector('.zoom-in');
+    let zoomOut = document.querySelector('.zoom-out');
+    
+    let drawing = false;
+
+    pencil.addEventListener('click', () => {
+        canvas.addEventListener('mousedown', startDrawing);
+        canvas.addEventListener('mousemove', draw);
+        canvas.addEventListener('mouseup', stopDrawing);
+        canvas.addEventListener('mouseout', stopDrawing);
+
+        canvas.addEventListener('touchstart', startDrawing);
+        canvas.addEventListener('touchmove', draw);
+        canvas.addEventListener('touchend', stopDrawing);
+        canvas.addEventListener('touchcancel', startDrawing);
+    });
+
+    function getEventPosition(event) {
+        if(event.touches) {
+            return {
+                x: event.touches[0].clientX - canvas.getBoundingClientRect().left,
+                y: event.touches[0].clientY - canvas.getBoundingClientRect().top
+            };
+        }else {
+            return{
+                x: event.offsetX,
+                y: event.offsetY
+            };
+        }
+    }
+
+    function startDrawing(event) {
+        drawing = true;
+        const pos = getEventPosition(event);
+        context.beginPath();
+        context.moveTo(pos.x, pos.y);
+        event.preventDefault();
+    }
+
+    function draw(event) {
+        if (!drawing) return;
+        const pos = getEventPosition(event)
+        context.lineTo(pos.x, pos.y);
+        context.stroke();
+        event.preventDefault();
+    }
+
+    function stopDrawing() {
+        drawing = false;
+        context.closePath();
+    }
+}
+
+tools();
+
